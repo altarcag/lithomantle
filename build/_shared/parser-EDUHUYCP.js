0cf305f057b5399912d3342be4cc818e590ec0dd
Taha Altar Çağ <taha.cag@stud.uni-goettingen.de>
2026-06-17 01:59:31 +0200

    Update documentation

--- build/_shared/parser-EDUHUYCP.js
+++ build/_shared/parser-EDUHUYCP.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-XJ5BVEFG.js";import"/build/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/lithomantle/build/_shared/chunk-XJ5BVEFG.js";import"/lithomantle/build/_shared/chunk-OZE3FFNP.js";export default a();
