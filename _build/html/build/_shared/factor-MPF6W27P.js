c69ab68fe2ead9d92fa004a5281095f6264764d6
Taha Altar Çağ <taha.cag@stud.uni-goettingen.de>
2026-06-17 01:58:50 +0200

    17062026v2

--- _build/html/build/_shared/factor-MPF6W27P.js
+++ _build/html/build/_shared/factor-MPF6W27P.js
@@ -1,1 +1,1 @@
-import{a}from"/build/_shared/chunk-4VF2MTNC.js";import"/build/_shared/chunk-OZE3FFNP.js";export default a();
+import{a}from"/lithomantle/build/_shared/chunk-4VF2MTNC.js";import"/lithomantle/build/_shared/chunk-OZE3FFNP.js";export default a();
